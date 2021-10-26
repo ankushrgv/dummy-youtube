1250a2ee993939b3d71efaa1b5211eccd9f54482
Ankush Raghuvanshi <araghuvanshi@microsoft.com>
2021-10-26 18:05:53 +0530

    Added basic layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { Container, Grid } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Container maxWidth="xl">
+        <Grid container spacing={2}>
+          <Grid item md={9} xs={12}>
+            <Grid container spacing={2}>
+              <Grid item md={9} xs={12}>
+                Video Container
+              </Grid>
+              <Grid item md={12} xs={12}>
+                Comments Section
+              </Grid>
+            </Grid>
+          </Grid>
+          <Grid item md={3} xs={12}>
+            Video Recommendations
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
